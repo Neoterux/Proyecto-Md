46d67ce9917e96c18e8b0de1d0cb02b77dcae219
Neoterux <labfernandez2014@gmail.com>
2022-01-26 00:44:41 -0500

    Added Methods and force exit for end threads

--- src/main/java/com/neoterux/pmd/MainApp.java
+++ src/main/java/com/neoterux/pmd/MainApp.java
@@ -4,36 +4,54 @@ import com.neoterux.pmd.controllers.MainController;
 import com.neoterux.pmd.controllers.OwnerWindowController;
 import com.neoterux.server.api.ServerManager;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import static com.neoterux.pmd.utils.JfxUtils.getLoaderOf;
 
 public class MainApp extends Application {
-
-    public static void main(String[] args) {
+    
+    /**
+     * The root window of the Application
+     */
+    private static Stage rootStage;
+    
+    public static void main (String[] args) {
         launch(args);
     }
-
+    
+    /**
+     * Gets the root Stage of teh Javafx Application
+     *
+     * @return The window
+     */
+    public static Stage getRootStage () {
+        return rootStage;
+    }
+    
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start (Stage primaryStage) throws Exception {
+        rootStage = primaryStage;
         try {
             var loader = getLoaderOf("main_window.fxml");
             loader.setControllerFactory(param -> new MainController(primaryStage));
-            //Scene mainScene = new Scene(loadFxml("main_window"));
             Scene mainScene = new Scene(loader.load());
             primaryStage.setScene(mainScene);
             primaryStage.show();
         } catch (NullPointerException npe) {
-    
+            System.out.println("This should not execute!!");
+            npe.printStackTrace();
         }
     }
     
     @Override
-    public void stop () throws Exception {
-        System.out.println("A");
+    public void stop () {
+        System.out.println("Closing application");
         OwnerWindowController.workers.shutdownNow();
         ServerManager.closeInstances();
-        super.stop();
+        System.out.println("Threads closed");
+        Platform.exit();
+        System.exit(0);
     }
 }
